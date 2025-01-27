0b731f8c242dd80f19eb05c62246eaad9f18e637
нурия клементьева <cnuria@mail.ru>
2025-01-27 19:01:31 +0500

    дополнение кода(пробел в имени)

--- src/widget.py
+++ src/widget.py
@@ -1,4 +1,4 @@
-def mask_account_card (number_card: str) -> str:
+def mask_account_card(number_card: str) -> str:
     """Функция, возвращающая замаскированный номер карты или счёта"""
 
     schet = "Счет"
@@ -9,11 +9,12 @@ def mask_account_card (number_card: str) -> str:
         name_card = []
         for i in list_name_card:
             if i.isalpha():
-                name_card+=i
+                name_card += i
             elif i.isdigit():
                 numbers_card = i
-        return f"{"".join(name_card)} {numbers_card[0:4]} {numbers_card[4:6]}** **** {numbers_card[-4:]}"
+        return f"{" ".join(list_name_card[:-1])} {numbers_card[0:4]} {numbers_card[4:6]}** **** {numbers_card[-4:]}"
+
 
 if __name__ == "__main__":
-    print(mask_account_card(str("Visa Platinum 2202345612340099")))
+    print(mask_account_card(str("Maestro 2202345612340099")))
     print(mask_account_card(str("Счет 11223344556677889900")))
