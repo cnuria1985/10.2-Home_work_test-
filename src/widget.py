6cd581d2582fce3c9f4ce59a2bdf2ce869175246
нурия клементьева <cnuria@mail.ru>
2025-01-31 22:37:03 +0500

    docstring

--- src/widget.py
+++ src/widget.py
@@ -14,14 +14,14 @@ def mask_account_card(number_card: str) -> str:
                 numbers_card = i
         return f"{" ".join(list_name_card[:-1])} {numbers_card[0:4]} {numbers_card[4:6]}** **** {numbers_card[-4:]}"
 
-def get_date (date_init: str) -> str:
+
+def get_date(date_init: str) -> str:
+    "Функция, форматирующая дату"
     use_date = f"{date_init[8:10]}.{date_init[5:7]}.{date_init[0:4]}"
     return use_date
+
 
 if __name__ == "__main__":
     print(mask_account_card(str("Visa Gold 5999414228426353")))
     print(mask_account_card(str("Счет 73654108430135874305")))
     print(get_date(str("2024-03-11T02:26:18.671407")))
-
-
-
