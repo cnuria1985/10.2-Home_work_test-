75cb6149abc406bef37ddb5fba779c2605056589
нурия клементьева <cnuria@mail.ru>
2025-11-28 17:22:34 +0500

    new code

--- src/processing.py
+++ src/processing.py
@@ -10,9 +10,15 @@ def filter_by_state(list_of_dictionaries: list[dict], state: str = "EXECUTED") -
     return total_of_dictionary
 
 
-
 def sort_by_date(list_of_dictionaries: list[dict], reverse: bool = True) -> list[dict]:
     """Функция возвращает список словарей, отсортированный по дате"""
 
     total_of_dictionary = sorted(list_of_dictionaries, key=lambda x: x["date"], reverse=reverse)
     return total_of_dictionary
+
+
+if __name__ == '__main__':
+    print(sort_by_date([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+                        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
+                        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+                        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]))
