aadad71300faf8c1089f057337ee4642721397e4
нурия клементьева <cnuria@mail.ru>
2026-01-31 23:17:34 +0500

    decorators

---
+++ src/decorators.py
@@ -0,0 +1,33 @@
+from time import time
+from functools import wraps
+import logging
+
+def log(filename=None):
+     def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            try:
+                result = func(*args, **kwargs)
+                if filename:
+                    with open(filename, "a", encoding="UTF-8") as file:
+                        file.write(f"{func.__name__} ok\n")
+                else:
+                    print(f"{func.__name__} ok\n")
+                return result
+            except Exception as e:
+                if filename:
+                    with open(filename, "a", encoding="UTF-8") as file:
+                        file.write(f'{func.__name__} error: {e}. Inputs: {args} {kwargs}\n')
+                else:
+                    print(f'{func.__name__} error: {e}. Inputs: {args} {kwargs}')
+
+        return wrapper
+     return decorator
+
+
+@log()
+def my_function(x, y):
+    return x + y
+
+
+my_function('1', 2)
