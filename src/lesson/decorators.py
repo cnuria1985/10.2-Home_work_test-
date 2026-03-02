eb074ee7232cc923126b84fa7b85d94c4e955500
нурия клементьева <cnuria@mail.ru>
2026-03-02 14:46:17 +0500

    convert

---
+++ src/lesson/decorators.py
@@ -0,0 +1,32 @@
+
+from functools import wraps
+
+
+def log(filename=None):
+     """Декоратор, принимающий аргумент файл и возвращает результат функции либо
+     тип ошибки с ипользованными аргументами"""
+     def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            try:
+                result = func(*args, **kwargs)
+                if filename:
+                    with open(filename, "a", encoding="UTF-8") as file:
+                        file.write(f"{func.__name__} ok\n")
+                else:
+                    print(f"{func.__name__} ok\n")
+                return result
+            except Exception as e:
+                if filename:
+                    with open(filename, "a", encoding="UTF-8") as file:
+                        file.write(f'{func.__name__} error: {e}. Inputs: {args} {kwargs}\n')
+                else:
+                    print(f'{func.__name__} error: {e}. Inputs: {args} {kwargs}')
+        return wrapper
+     return decorator
+
+
+
+
+
+
